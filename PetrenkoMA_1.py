15ac0410c27c6881e27bb091f767dc903ee1d948
MikhailPetrenko1 <149270814+MikhailPetrenko1@users.noreply.github.com>
2024-06-21 11:07:03 +0300

    Add files via upload

---
+++ PetrenkoMA_1.py
@@ -0,0 +1,7 @@
+# Программа выведет значение, тип и место в памяти вещественного числа.
+var_int = 5
+var_float = 3.14
+var_str = 'Mikhail'
+var_bool = True
+var_comlex = 1 - 3j
+print(var_float,type(var_float),id(var_float))
